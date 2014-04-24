5d2603797b9e4da97e53ed4b7c6e26fc76bc2f2e
jameskoster <jameskoster@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-24 12:06:44 +0000

    releasing 1.2.1
    git-svn-id: https://plugins.svn.wordpress.org/projects-by-woothemes/tags/1.2.1@901637 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ projects.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name: Projects
+ * Plugin URI: http://woothemes.com/
+ * Description: Hi, I'm your project showcase plugin for WordPress. Show off your recent work using our shortcode, widget or template tag.
+ * Author: WooThemes
+ * Version: 1.2.1
+ * Author URI: http://woothemes.com/
+ *
+ * @package WordPress
+ * @subpackage Projects
+ * @author Matty
+ * @since 1.0.0
+ */
+
+require_once( 'classes/class-projects.php' );
+require_once( 'classes/class-projects-taxonomy.php' );
+require_once( 'classes/class-projects-shortcodes.php' );
+require_once( 'classes/class-projects-settings.php' );
+require_once( 'classes/class-widget-projects.php' );
+require_once( 'classes/class-widget-project-categories.php' );
+require_once( 'classes/class-projects-integrations.php' );
+
+require_once( 'projects-template.php' );
+require_once( 'projects-core-functions.php' );
+require_once( 'projects-hooks.php' );
+
+global $projects;
+$projects = new Projects( __FILE__ );
